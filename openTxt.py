071558b2b1eeb6fbdf41a93b966120614570c112
QendrimVllasa <qendrimvllasa@hotmail.de>
2019-04-17 01:15:57 +0200

    update

--- openTxt.py
+++ openTxt.py
@@ -1,19 +1,17 @@
 
 accounts = {}
-
+a = 0
+s = []
 with open('accountLists.txt', 'r+') as f:
     s = []
 
     for i in f:
-        if '[' and '='in i:
-            s = i.strip().split(' ')
-        if ']' in i:
-            l = i.strip()
-            s.append(l)
-        if not '[' in i:
-            if not ']' in i:
-                account = i.strip()
-                print(account)
+        if not i:
+            a += 1
+
+
+
+
 
 
 
