a588e85f20148a6c926dec6365df0c033041e422
QendrimVllasa <qendrimvllasa@hotmail.de>
2019-05-05 23:49:10 +0200

    update
    Signed-off-by: QendrimVllasa <qendrimvllasa@hotmail.de>

--- main_macOS.py
+++ main_macOS.py
@@ -223,6 +223,7 @@ class Worker(QThread):
 
     def changeIOT(self, usr):
 
+
         page = "https://academy2.eu1.mindsphere.io/index.html#/"
         if self.currentBrowser == 'Chrome':
             optionsChrome = webdriver.ChromeOptions()
@@ -242,7 +243,7 @@ class Worker(QThread):
                 executable_path=resource_path('webdriver/macOS/geckodriver'),
                 options=optionsFirefox,
             )
-
+        driver.delete_all_cookies()
         driver.implicitly_wait(3)
         # driver.minimize_window()
         driver.get(page)
@@ -253,27 +254,31 @@ class Worker(QThread):
         driver.find_element(By.XPATH, "//fieldset[@id='dr_signin']/div[3]/div/input").send_keys(self.newPassword)
         time.sleep(1)
         driver.find_element(By.XPATH, "//button[@id='login-button']").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH,
                             "//div[@id='_mscontent']/mindsphere-app-root/div/mindsphere-launchpad-container/div/div/div/div[6]/div/mindsphere-launchpad-tile-container/a/div").click()
+        time.sleep(2)
+        driver.refresh()
+
+
 
         driver.find_element(By.XPATH, "(//input[@name='user'])[2]").send_keys(usr)
 
         driver.find_element(By.XPATH, "(//input[@name='password'])[2]").send_keys(self.currentPassword)
-
+        time.sleep(1)
         driver.find_element(By.XPATH, "(//button[@type='submit'])[5]").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//div[@id='app']/div/c8y-ui-header/div/div/div[3]/button/span").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//div[@id='app']/div/c8y-ui-header/div/div/div[3]/ul/li/a").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//div[9]/button/span").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//input[@name='password']").send_keys(self.newPassword)
         driver.find_element(By.XPATH, "//input[@name='confirmPassword']").send_keys(self.newPassword)
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//div[3]/button[2]").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//input[@name='currentPassword']").send_keys(self.currentPassword)
         driver.find_element(By.XPATH, "//button").click()
         driver.delete_all_cookies()
@@ -438,8 +443,10 @@ class Worker(QThread):
 
 
                             # ------
-                            if self.iot == '2':
-                                self.changeIOT(user)
+                            if 'connectivity' in user or 'dev' in user:
+                                if self.iot == '2':
+                                    self.changeIOT(user)
+
                             # ------
 
                             print(str(count) + '%')
--- main_windows.py
+++ main_windows.py
@@ -242,7 +242,7 @@ class Worker(QThread):
                 executable_path=resource_path('webdriver/macOS/geckodriver'),
                 options=optionsFirefox,
             )
-
+        driver.delete_all_cookies()
         driver.implicitly_wait(3)
         # driver.minimize_window()
         driver.get(page)
@@ -253,27 +253,31 @@ class Worker(QThread):
         driver.find_element(By.XPATH, "//fieldset[@id='dr_signin']/div[3]/div/input").send_keys(self.newPassword)
         time.sleep(1)
         driver.find_element(By.XPATH, "//button[@id='login-button']").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH,
                             "//div[@id='_mscontent']/mindsphere-app-root/div/mindsphere-launchpad-container/div/div/div/div[6]/div/mindsphere-launchpad-tile-container/a/div").click()
+        time.sleep(2)
+        driver.refresh()
+
+
 
         driver.find_element(By.XPATH, "(//input[@name='user'])[2]").send_keys(usr)
 
         driver.find_element(By.XPATH, "(//input[@name='password'])[2]").send_keys(self.currentPassword)
-
+        time.sleep(1)
         driver.find_element(By.XPATH, "(//button[@type='submit'])[5]").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//div[@id='app']/div/c8y-ui-header/div/div/div[3]/button/span").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//div[@id='app']/div/c8y-ui-header/div/div/div[3]/ul/li/a").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//div[9]/button/span").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//input[@name='password']").send_keys(self.newPassword)
         driver.find_element(By.XPATH, "//input[@name='confirmPassword']").send_keys(self.newPassword)
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//div[3]/button[2]").click()
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.XPATH, "//input[@name='currentPassword']").send_keys(self.currentPassword)
         driver.find_element(By.XPATH, "//button").click()
         driver.delete_all_cookies()
@@ -437,8 +441,9 @@ class Worker(QThread):
 
 
                             #------
-                            if self.iot == '2':
-                                self.changeIOT(user)
+                            if 'connectivity' in user or 'dev' in user:
+                                if self.iot == '2':
+                                    self.changeIOT(user)
                             #------
 
 
